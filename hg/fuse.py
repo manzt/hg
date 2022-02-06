9f91b0abe661d7cd61af6dcf35df8f14a6b5eb7d
Trevor Manz <trevor.j.manz@gmail.com>
2022-02-06 16:50:17 -0500

    feat: add FuseProcess

--- hg/fuse.py
+++ hg/fuse.py
@@ -1,3 +1,10 @@
+import multiprocessing as mp
+import os
+from time import time
+from typing import Optional
+from urllib.parse import urlparse
+
+import fsspec.fuse
 from fsspec.asyn import sync_wrapper
 from fsspec.implementations.http import HTTPFileSystem
 
@@ -39,6 +46,55 @@ class GlobalHTTPFileSystem(HTTPFileSystem):
         # override HTTPFile/HTTPStreamFile url with real url
         file.url = path_to_url(file.path)
         return file
+
+
+class FuseProcess:
+    def __init__(self):
+        self._p: Optional[mp.Process] = None
+        self._mount_point: Optional[str] = None
+
+    def start(self, mount_point: str):
+        # no need to restart
+        if self._p and mount_point == self._mount_point:
+            return
+
+        self.stop()
+
+        mount_point = mount_point.rstrip("/") + "/"
+        assert os.path.isabs(mount_point), "must provide an absolute path"
+        assert os.path.isdir(
+            mount_point
+        ), f"mount directory doesn't exist: {mount_point}"
+
+        self._p = mp.Process(
+            target=fsspec.fuse.run,
+            args=(GlobalHTTPFileSystem(), "/", mount_point),
+            kwargs={"ready_file": True},
+            daemon=True,  # TODO: hmm, this doesn't seem to kill the underlying process
+        )
+
+        max_iters = 1000
+        for i in range(max_iters):
+
+            if os.path.exists(os.path.join(mount_point, ".fuse_ready")):
+                break
+
+            if i == max_iters - 1:
+                self.stop()
+                raise RuntimeError("Failed to setup FUSE")
+
+            time.sleep(0.5)
+
+    def stop(self):
+        if self._p:
+            self._p.terminate()
+            self._p = None
+
+        # TODO: more teardown, fuser
+
+    def path(self, href: str):
+        url = urlparse(href)
+        return f"/{url.scheme}/{url.netloc}{url.path}.."
 
 
 def path_to_url(path: str) -> str:
