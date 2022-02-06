22ddbd1ba443bfa809fdddfaac5a7728883234c5
Trevor Manz <trevor.j.manz@gmail.com>
2022-02-06 14:29:00 -0500

    fix: use fs name for file info, not url

--- hg/fuse.py
+++ hg/fuse.py
@@ -2,7 +2,46 @@ from fsspec.asyn import sync_wrapper
 from fsspec.implementations.http import HTTPFileSystem
 
 
-def to_url(path: str) -> str:
+class GlobalHTTPFileSystem(HTTPFileSystem):
+    def _cat_file(self, url, **kwargs):
+        url = path_to_url(url)
+        return super()._cat_file(url, **kwargs)
+
+    cat_file = sync_wrapper(_cat_file)
+
+    async def _info(self, path, **kwargs):
+        if self.isdir(path):
+            return direntry(path)
+        url = path_to_url(path)
+        entry = await super()._info(url, **kwargs)
+        # rename name with filesystem name (not url)
+        entry["name"] = path
+        return entry
+
+    info = sync_wrapper(_info)
+
+    def ls(self, path, detail=False, **kwargs):
+        if not self.isdir(path):
+            raise NotADirectoryError(path)
+        if path != "/":
+            return []
+        dirs = ["http/", "https/"]
+        if detail:
+            return [direntry(n) for n in dirs]
+        return dirs
+
+    def isdir(self, path):
+        return path[-2:] != ".."
+
+    # modified from fsspec.implementations.http.HTTPFileSystem
+    def _open(self, path, **kwargs):
+        file = super()._open(path, **kwargs)
+        # override HTTPFile/HTTPStreamFile url with real url
+        file.url = path_to_url(file.path)
+        return file
+
+
+def path_to_url(path: str) -> str:
     if path[-2:] != "..":
         raise FileNotFoundError(path)
 
@@ -23,38 +62,3 @@ def direntry(name: str):
         "size": None,
         "type": "directory",
     }
-
-
-class GlobalHTTPFileSystem(HTTPFileSystem):
-    def _cat_file(self, url, **kwargs):
-        url = to_url(url)
-        return super()._cat_file(url, **kwargs)
-
-    cat_file = sync_wrapper(_cat_file)
-
-    async def _info(self, path, **kwargs):
-        if self.isdir(path):
-            return direntry(path)
-        url = to_url(path)
-        return await super()._info(url, **kwargs)
-
-    info = sync_wrapper(_info)
-
-    def ls(self, path, detail=False, **kwargs):
-        if not self.isdir(path):
-            raise NotADirectoryError(path)
-        if path != "/":
-            return []
-        dirs = ["http/", "https/"]
-        if detail:
-            return [direntry(n) for n in dirs]
-        return dirs
-
-    def isdir(self, path):
-        return path[-2:] != ".."
-
-    # modified from fsspec.implementations.http.HTTPFileSystem
-    def _open(self, path, **kwargs):
-        f = super()._open(path, **kwargs)
-        f.url = to_url(f.path)
-        return f
